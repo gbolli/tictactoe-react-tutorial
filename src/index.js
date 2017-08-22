8b224b026ad3c0a87bc0503f699ab73195ee48a5
Gord Bolli <gord@bolli.ca>
2017-08-22 14:27:21 -0700

    added click event to add X and move state to Board class

--- src/index.js
+++ src/index.js
@@ -3,18 +3,37 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()  // copy array to new variable
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
